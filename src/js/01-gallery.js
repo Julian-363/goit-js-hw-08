1e413617f84056aec430c078ca8f9af00f3f842a
Julian-363 <juliangil333@gmail.com>
2023-05-21 21:09:44 -0500

    first commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,8 @@
+// Descrito en la documentación
+import SimpleLightbox from 'simplelightbox';
+// Importación adicional de estilos
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items';
+
+console.log(galleryItems);
